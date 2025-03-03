4dbb99783b2d233d44a1a68f7982d6d935647dc2
Administrator <2139026822@qq.com>
2025-03-03 17:21:12 +0800

    初始化

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 from fastapi import FastAPI
 
 from app.database import create_all_tables
-from app.user_app import routers as user_routers
-from app.Cost_app import routers as cost_routers
-from app.file_manage import routers as file_routers
 
 
 
@@ -12,13 +9,6 @@ app = FastAPI()
 # 初始化表
 create_all_tables()
 
-# app.include_router(
-#     router=user_routers.router, tags=["auth"]
-#
-# )
-# app.include_router(
-#     router=cost_routers.router, tags=["cost"]
-# )
 app.include_router(
     router=file_routers.router, tags=["file"]
 )
